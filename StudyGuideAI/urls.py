c8997fcfa96019381d812eb1ebca516b61f3486f
FirdausFadlizam <119713495+FirdausFadlizam@users.noreply.github.com>
2023-10-22 02:35:56 -0400

    Add files via upload

--- StudyGuideAI/urls.py
+++ StudyGuideAI/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.upload_material, name='upload_material'),  # Define a URL pattern for the root path
     path('upload/', views.upload_material, name='upload_material'),  # This line is optional
+    path('download_pdf/', views.download_pdf, name='download_pdf'),
+    #path('download_pdf2/', views.download_pdf2, name='download_pdf2'),
+
     # Add more URL patterns as needed for your app
 ]
