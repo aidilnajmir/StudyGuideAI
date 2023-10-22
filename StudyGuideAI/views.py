5f13bccf0a1cc961a4bbffeb2dc2a52b0100ec13
gesionglau <101308014+gesionglau@users.noreply.github.com>
2023-10-22 16:37:54 +0800

    Update views.py

--- StudyGuideAI/views.py
+++ StudyGuideAI/views.py
@@ -36,7 +36,7 @@ def extract_text(uploaded_file):
     return text
 
 def summarize_text(text):
-    openai.api_key = 'INSERT API KEY'
+    openai.api_key = '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'
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=f"Provide key points of this text in bullet points and 10 multiple choice questions with answers (add <br /> as string after each keypoint,question,answer choice, and answer. add <br /> as string before each question add <br /> as string before the first keypoint. add <br /> <br /> as string after the last keypoint <): '{text}'",
@@ -79,32 +79,39 @@ def download_pdf(request):
     return response
 
 
-# def string_to_pdf2(pdf_content):
+def string_to_pdf2(pdf_content):
    
-#    # Create a BytesIO buffer to store the PDF content
-#     pdf_buffer = BytesIO()
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'inline; filename="output.pdf"'
+    response['X-Frame-Options'] = 'ALLOWALL'
 
-#     # Create a PDF document
-#     c = canvas.Canvas(pdf_buffer)
-#     c.drawString(100, 750, pdf_content)
+    doc = SimpleDocTemplate(response, pagesize=letter)
+    elements = []
 
-#     # End the PDF document
-#     c.showPage()
-#     c.save()
 
-#     # Reset the buffer position to the beginning
-#     pdf_buffer.seek(0)
+    # Define a paragraph style for the content
+    styles = getSampleStyleSheet()
+    content_style = ParagraphStyle(
+        name='ContentStyle',
+        parent=styles['Normal'],
+        fontSize=12,
+        leading=14,
+    )
 
-#     # Serve the PDF as an attachment with the appropriate headers
-#     response = FileResponse(pdf_buffer, content_type='application/pdf')
-#     response['Content-Disposition'] = 'inline; filename="output.pdf"'
+    # Create a Paragraph with the provided text and style
+    content = Paragraph(pdf_content, content_style)
+    elements.append(content)
 
-#     return response
+    # Build the PDF document
+    doc.build(elements)
+
+    return response
+    # Serve the PDF as an attachment with the appropriate headers
    
 
-# def download_pdf2(request):
-#     global summarized_text
-#     response = string_to_pdf2(summarized_text)
-#     return response
+def download_pdf2(request):
+    global summarized_text
+    response = string_to_pdf2(summarized_text)
+    return response
 
 
